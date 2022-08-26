9c0264f8340c1093257fc37e0058dec58662aa64
MrN1x0n <95374645+MrN1x0n@users.noreply.github.com>
2022-08-26 16:46:48 +0300

    Update sui-update.sh

--- sui-update.sh
+++ sui-update.sh
@@ -1,7 +1,12 @@
+#update
 sudo apt update
-sudo apt install cargo
+sudo apt install cargo -y
+
+#stop and remove db
 systemctl stop suid
 rm -rf $HOME/.sui/db
+
+#get new  genesis
 wget -qO $HOME/.sui/genesis.blob https://github.com/MystenLabs/sui-genesis/raw/main/devnet/genesis.blob
 cd $HOME/sui
 git remote add upstream https://github.com/MystenLabs/sui
@@ -10,3 +15,8 @@ git stash
 git checkout -B devnet --track upstream/devnet
 cargo build --release
 mv $HOME/sui/target/release/{sui,sui-node,sui-faucet} /usr/bin/
+
+#check version
+sui -V
+#restart
+systemctl restart suid
