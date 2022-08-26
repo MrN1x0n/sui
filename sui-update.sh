fb62255ad62200d316857978381e52b3d94146db
MrN1x0n <95374645+MrN1x0n@users.noreply.github.com>
2022-08-26 16:27:14 +0300

    Create sui-update.sh

---
+++ sui-update.sh
@@ -0,0 +1,10 @@
+systemctl stop suid
+rm -rf $HOME/.sui/db
+wget -qO $HOME/.sui/genesis.blob https://github.com/MystenLabs/sui-genesis/raw/main/devnet/genesis.blob
+cd $HOME/sui
+git remote add upstream https://github.com/MystenLabs/sui
+git fetch upstream
+git stash
+git checkout -B devnet --track upstream/devnet
+cargo build --release
+mv $HOME/sui/target/release/{sui,sui-node,sui-faucet} /usr/bin/
